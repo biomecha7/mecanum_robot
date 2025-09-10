015bdf6d3bc4c0fdc73b0de88a096141b5c2aaec
biorobobuilder <victorstepanov2019@u.northwestern.edu>
2025-09-09 21:23:26 -0400

    Implement EncoderDriver

--- include/EncoderDriver.h
+++ include/EncoderDriver.h
@@ -1,12 +1,22 @@
 #pragma once
 #include <Arduino.h>
 
+
 class EncoderDriver {
 public:
     EncoderDriver(uint8_t pinA, uint8_t pinB, volatile int32_t* countPtr);
     void begin();
+
+    // For ISR routing
+    static void isr0();
+    static void isr1();
+    static void isr2();
+    static void isr3();
+
 private:
     uint8_t _pinA, _pinB;
     volatile int32_t* _countPtr;
-    static void isrA();
+    int _index;
+    void handleISR();
+    static EncoderDriver* instances[4];
 };
--- src/EncoderDriver.cpp
+++ src/EncoderDriver.cpp
@@ -1,15 +1,58 @@
 #include "EncoderDriver.h"
 
+
+EncoderDriver* EncoderDriver::instances[4] = {nullptr, nullptr, nullptr, nullptr};
+
 EncoderDriver::EncoderDriver(uint8_t pinA, uint8_t pinB, volatile int32_t* countPtr)
-    : _pinA(pinA), _pinB(pinB), _countPtr(countPtr) {}
+    : _pinA(pinA), _pinB(pinB), _countPtr(countPtr), _index(-1) {}
 
 void EncoderDriver::begin() {
     pinMode(_pinA, INPUT_PULLUP);
     pinMode(_pinB, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(_pinA), isrA, RISING);
+
+    // Find first available slot
+    for (int i = 0; i < 4; ++i) {
+        if (instances[i] == nullptr) {
+            instances[i] = this;
+            _index = i;
+            break;
+        }
+    }
+
+    // Attach correct ISR
+    switch (_index) {
+        case 0:
+            attachInterrupt(digitalPinToInterrupt(_pinA), isr0, RISING);
+            break;
+        case 1:
+            attachInterrupt(digitalPinToInterrupt(_pinA), isr1, RISING);
+            break;
+        case 2:
+            attachInterrupt(digitalPinToInterrupt(_pinA), isr2, RISING);
+            break;
+        case 3:
+            attachInterrupt(digitalPinToInterrupt(_pinA), isr3, RISING);
+            break;
+    }
 }
 
-void EncoderDriver::isrA() {
-    // This is a placeholder. Actual implementation should handle multiple instances.
-    // For a real modular approach, use static array of instances or function pointers.
+void EncoderDriver::handleISR() {
+    if (digitalRead(_pinB)) {
+        (*_countPtr)++;
+    } else {
+        (*_countPtr)--;
+    }
+}
+
+void EncoderDriver::isr0() {
+    if (instances[0]) instances[0]->handleISR();
+}
+void EncoderDriver::isr1() {
+    if (instances[1]) instances[1]->handleISR();
+}
+void EncoderDriver::isr2() {
+    if (instances[2]) instances[2]->handleISR();
+}
+void EncoderDriver::isr3() {
+    if (instances[3]) instances[3]->handleISR();
 }
